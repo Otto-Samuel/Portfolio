6fbc0646e19598dd0dbd4b68384a1f8b69fad9ad
Otto Samuel <ottosamuel01@gmail.com>
2025-10-12 10:40:35 -0300

    melhorando as animacoes dos botoes

--- Paginacertificados.js
+++ Paginacertificados.js
@@ -1,3 +1,11 @@
 function voltar(){
     window.location.href = "home.html";
 }
+
+const botoes = document.querySelectorAll('.btn-inst');
+botoes.forEach(btn => {
+  btn.addEventListener('click', () => {
+    botoes.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
