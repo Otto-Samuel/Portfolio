24e4821de7d31a7a91c06c9c58d91b2ccebbb2ad
Otto Samuel <ottosamuel01@gmail.com>
2025-10-12 11:27:00 -0300

    adicionando certificados em grid

--- Paginacertificados.js
+++ Paginacertificados.js
@@ -1,3 +1,8 @@
+const certImgs = document.querySelectorAll('.cert-img');
+const modal = document.getElementById('pdfModal');
+const pdfViewer = document.getElementById('pdfViewer');
+const closeModal = document.querySelector('.close-modal');
+
 function voltar(){
     window.location.href = "home.html";
 }
@@ -8,4 +13,26 @@ botoes.forEach(btn => {
     botoes.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
   });
+});
+
+
+
+certImgs.forEach(img => {
+  img.addEventListener('click', () => {
+    const pdfUrl = img.getAttribute('data-pdf');
+    pdfViewer.src = pdfUrl;
+    modal.style.display = 'flex';
+  });
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+  pdfViewer.src = ''; // limpa o iframe
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+    pdfViewer.src = '';
+  }
 });
